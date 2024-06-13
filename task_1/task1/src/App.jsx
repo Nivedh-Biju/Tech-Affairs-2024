f14edd86ea432cf437a911eca3380367213f7443
AdityaSaiKonduri <adityakonduri8@gmail.com>
2024-06-13 17:53:25 +0530

    Carousel

--- task_1/task1/src/App.jsx
+++ task_1/task1/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import './App.css';
 import SamplePage from './pages/samplepage/samplepage';
+import carouseldata from "./carouselData.json"
 
 function App() {
+  let { slides } = carouseldata;
+  // console.log(slides);
   return (
     <Router> 
-      <div className="App">
+      <div className="dark:bg-slate-800 dark:text-white m-0 min-h-screen">
         <Routes> 
-          <Route path="/" element={<SamplePage />} /> 
+          <Route path="/" element={<SamplePage data = {slides}/>} /> 
         </Routes>
       </div> 
     </Router>
