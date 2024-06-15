3dd1868b2778ac90487a86b86f7df97cf23c8649
AdityaSaiKonduri <adityakonduri8@gmail.com>
2024-06-15 16:13:10 +0530

    Github link and more styling

--- cs22b2012_task_2/src/components/ResultPage.jsx
+++ cs22b2012_task_2/src/components/ResultPage.jsx
@@ -16,10 +16,16 @@ const ResultPage = () => {
   }, []);
 
   return (
-    <div className='flex flex-col items-center justify-center p-4 gap-5'>
-      <h1 className='text-3xl mb-4'>Quiz Results</h1>
+    <div className='flex flex-col items-center h-[500px] justify-center p-4 gap-5'>
+      <h1 className='text-2xl sm:text-3xl lg:text-4xl mb-4'>Quiz Results</h1>
       <p className='text-2xl'>Number of correct answers: {correctAnswers} / {questions.length}</p>
-      <Link to={"/quiz"}><button className=' hover:bg-orange-400 p-2 sm:p-3 button-color rounded-3xl'>Retry Quiz</button></Link>
+      <Link to={"/quiz"}><button className='hover:bg-orange-400 p-2 sm:p-3 lg:px-4  button-color rounded-3xl p-2'>Retry Quiz</button></Link>
+      <a href="https://github.com/AdityaSaiKonduri/Tech-Affairs-2024-Tasks" target='_blank' className='underline-none w-auto flex items-center justify-center gap-4 hover:text-yellow-600'>
+        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961" />
+        </svg>
+        Link to Github Repository
+      </a>
     </div>
   );
 };
