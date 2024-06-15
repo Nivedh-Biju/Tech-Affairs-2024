d1e9c4a0aa15e8621f593a8298f2e2da471a370b
AdityaSaiKonduri <adityakonduri8@gmail.com>
2024-06-15 16:21:20 +0530

    Task 2: Randomized questions

--- cs22b2012_task_2/src/components/QuizPage.jsx
+++ cs22b2012_task_2/src/components/QuizPage.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 };
