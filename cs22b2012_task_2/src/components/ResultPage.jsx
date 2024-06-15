7119a8023a804822003a7600aa06829fbf5a59a3
AdityaSaiKonduri <adityakonduri8@gmail.com>
2024-06-16 01:17:11 +0530

    Task 2 : Few deletions

--- cs22b2012_task_2/src/components/ResultPage.jsx
+++ cs22b2012_task_2/src/components/ResultPage.jsx
@@ -18,7 +18,7 @@ const ResultPage = () => {
   return (
     <div className='flex flex-col items-center h-[500px] justify-center p-4 gap-5'>
       <h1 className='text-2xl sm:text-3xl lg:text-4xl mb-4'>Quiz Results</h1>
-      <p className='text-2xl'>Number of correct answers: {correctAnswers} / {questions.length}</p>
+      <p className='text-xl sm:text-2xl'>Number of correct answers: {correctAnswers} / {questions.length}</p>
       <Link to={"/quiz"}><button className='hover:bg-orange-400 p-2 sm:p-3 lg:px-4  button-color rounded-3xl p-2'>Retry Quiz</button></Link>
       <a href="https://github.com/AdityaSaiKonduri/Tech-Affairs-2024-Tasks" target='_blank' className='underline-none w-auto flex items-center justify-center gap-4 hover:text-yellow-600'>
         <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
